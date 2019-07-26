311d7b9e219c8d8369abd717bd88d39aebf2046b
lisijing <15564641022@163.com>
2019-07-26 09:24:28 +0800

    springboot升级改造

---
+++ nk_wms/src/main/java/com/hs/nkwms/NkwmsApplication.java
@@ -0,0 +1,16 @@
+package com.hs.nkwms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动入口 newport
+ */
+@SpringBootApplication
+public class NkwmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NkwmsApplication.class, args);
+    }
+
+}
